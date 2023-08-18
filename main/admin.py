390008e12f36cdbe8a59309d804bc338913fbaea
BaLaMyt900 <balamyt900@gmail.com>
2023-08-18 13:58:05 +0400

    Добавлены модели

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+
+rom django.contrib import admin
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import string
+import random
 
-# Create your models here.
+
+def _generate_referal_code() -> str:
+    """ Генератор реферального кода """
+    letters_and_digits = string.ascii_letters + string.digits
+    while True:
+        code = ''.join(random.sample(letters_and_digits, 6))
+        """ Проверка на уникальность """
+        if not User.objects.get(referal_code=code).exists():
+            return code
+
+
+class User(AbstractUser):
+    """ Расширенная модель стандарного django пользователя
+     согласно тех. заданию"""
+    phone = models.IntegerField(unique=True, null=False)
+    referral_code = models.CharField(unique=True, max_length=6, default=_generate_referal_code())
+    user_referral_code = models.CharField(null=True, blank=True)
+
