ee5450e2af6a3274cb09f2b7245c6926bb6f7afb
BaLaMyt900 <balamyt900@gmail.com>
2023-08-18 16:02:12 +0400

    Добавлены модели

--- main/models.py
+++ main/models.py
@@ -1,23 +1,8 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-import string
-import random
 
 
-def _generate_referal_code() -> str:
-    """ Генератор реферального кода """
-    letters_and_digits = string.ascii_letters + string.digits
-    while True:
-        code = ''.join(random.sample(letters_and_digits, 6))
-        """ Проверка на уникальность """
-        if not User.objects.get(referal_code=code).exists():
-            return code
-
-
-class User(AbstractUser):
-    """ Расширенная модель стандарного django пользователя
-     согласно тех. заданию"""
+class User(models.Model):
+    """ Модель пользователя согласно тех. заданию"""
     phone = models.IntegerField(unique=True, null=False)
-    referral_code = models.CharField(unique=True, max_length=6, default=_generate_referal_code())
-    user_referral_code = models.CharField(null=True, blank=True)
-
+    user_referral_code = models.CharField(unique=True, max_length=6)
+    referral_code = models.CharField(null=True, blank=True, max_length=6)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import User
+import random
+import string
 
-# Create your views here.
+
+def generate_referal_code() -> str:
+    """ Генератор реферального кода """
+    letters_and_digits = string.ascii_letters + string.digits
+    while True:
+        code = ''.join(random.sample(letters_and_digits, 6))
+        """ Проверка на уникальность """
+        if not User.objects.get(user_referal_code=code).exists():
+            return code
+
+
+
